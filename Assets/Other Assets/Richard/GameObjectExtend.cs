1d938e3f11377ac29255b1e52fc1f303396f9264
Richard <rail.rich@gmail.com>
2015-02-02 15:23:26 -0500

    M<y shits

---
+++ Assets/Other Assets/Richard/GameObjectExtend.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public static class GameObjectExtend{
+
+	
+	public static GameObject createClone(GameObject original, string name, Transform parent, Vector3 position, bool translateToParent = false){
+		GameObject go = (GameObject)Object.Instantiate(original);
+		go.name = name;
+		go.transform.parent = parent;
+		if(translateToParent){
+			go.transform.position = parent.transform.position;
+		}
+		go.transform.Translate(position);
+		go.transform.Translate(original.transform.position);
+		
+		return go;
+	}
+	
+	public static GameObject createGameObject(string name, Transform parent, Vector3 position, bool translateToParent = false){
+		GameObject go = new GameObject();
+		go.name = name;
+		go.transform.parent = parent;
+		if(translateToParent){
+			go.transform.position = parent.transform.position;
+		}
+		
+		go.transform.Translate(position);
+		
+		return go;
+	}
+	
+	public static GameObject createGameObject(string name, Transform parent){
+		GameObject go = new GameObject();
+		go.name = name;
+		go.transform.parent = parent;
+		
+		return go;
+	}
+	
+	public static void deleteAllChilds(GameObject go){
+		var children = new List<GameObject>();
+		foreach (Transform child in go.transform) children.Add(child.gameObject);
+		children.ForEach(child => deleteGameObject(child));
+	}
+	
+	public static void deleteGameObject(GameObject go){
+		if(Application.isPlaying){
+			Object.Destroy(go);
+		}else{
+			Object.DestroyImmediate(go);
+		}
+	}
+}
---
+++ Assets/Scripts/Chunk/ChunkLoader/Editor/ChuckLoaderEditor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+
+[System.Serializable]
+public class ChuckLoaderEditor : EditorWindow {
+
+	public string filePath = "";
+	
+	void OnGUI(){	
+		addFileLine();
+		if (GUILayout.Button ("Load Map")) {
+			string filename = Path.GetFileName(filePath).Split(new char[]{'.'})[0];
+			GameObject tempParent = new GameObject(filename);
+			
+			ChunckLoader loader = new ChunckLoader(tempParent);
+			loader.loadFromFile(filePath);
+		}
+	}
+
+	/*GameObject addOrGetPrefab(){
+		
+		
+		GameObject go = Resources.Load<GameObject>("Chunks/" + filename);
+		
+		Object prefab = EditorUtility.CreateEmptyPrefab("Assets/Temporary/"+t.gameObject.name+".prefab");
+        EditorUtility.ReplacePrefab(t.gameObject, prefab, ReplacePrefabOptions.ConnectToPrefab);
+		
+		return null;
+	}*/
+	
+	void addFileLine(){
+		GUILayout.BeginHorizontal ();
+		filePath = GUILayout.TextField (filePath);
+		if (GUILayout.Button ("Open Map File")) {
+			filePath = EditorUtility.OpenFilePanel("Open Map File","maps","tmx");
+		}
+		GUILayout.EndHorizontal ();
+	}
+	
+	[MenuItem ("FruitsUtils/Map Loader")]
+	public static void ShowWindow(){
+		EditorWindow.GetWindow(typeof(ChuckLoaderEditor), true, "Map Loader");
+	}
+}
