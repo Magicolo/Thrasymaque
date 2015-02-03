f7940448fe2012ba60560dcf5ff1e72f6e323c5e
Magicolo <straty91@hotmail.com>
2015-02-03 12:29:25 -0500

    f

--- Assets/Scripts/RunnerCamera.cs
+++ Assets/Scripts/RunnerCamera.cs
@@ -7,6 +7,7 @@ public class RunnerCamera : MonoBehaviour {
 	public float translateSpeed = 5;
 	public float rotateSpeed = 5;
 	public float pressureSpeed = 10;
+	public float pressureThreshold = 0.9F;
 	public float recoverSpeed = 2;
 	public Vector3 offset = new Vector3(0, 0, -50);
 	public Vector3 lastPosition;
@@ -15,9 +16,9 @@ public class RunnerCamera : MonoBehaviour {
 		transform.RotateTowards(References.Runner.orientation, 5, "Z");
 		
 		float distance = Vector3.Distance(lastPosition, References.Runner.transform.position);
-		transform.TranslateTowards(References.Runner.transform.position + offset, translateSpeed);
+		transform.TranslateTowards(References.Runner.transform.localPosition + offset.Rotate(-transform.eulerAngles.z), translateSpeed);
 		
-		if (distance > 0.9F && offset.x > 0) {
+		if (distance > pressureThreshold && offset.x > 0) {
 			offset.x -= Time.fixedDeltaTime * recoverSpeed;
 		}
 		else {
